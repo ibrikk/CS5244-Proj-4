5b60b0e25c9661309381b15cbe16f2b8f38b4f01
Ibrahim Khalilov <ibrahim.khalilov@yahoo.com>
2024-10-03 21:43:15 -0400

    commit

--- app/src/main/java/api/ApiResource.java
+++ app/src/main/java/api/ApiResource.java
@@ -97,7 +97,6 @@ public class ApiResource {
         }
     }
 
-    // TODO Implement the following APIs
     @GET
     @Path("categories/name/{category-name}")
     @Produces(MediaType.APPLICATION_JSON)
